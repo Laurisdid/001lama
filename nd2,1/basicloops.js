df56572e1d8d28fb465fc1e9906928780b584c86
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-09 11:04:58 +0300

    push
    push

--- nd2,1/basicloops.js
+++ nd2,1/basicloops.js
@@ -22,8 +22,20 @@ while(uzd9<100){
 console.log('---------------------10----------------------------');
 let uzd10 = 0;
 let cikl10=0;
-while (uzd10 !==5 || uzd10 !== 7){
+while (uzd10 !==5 && uzd10 !== 7){
     uzd10 = rand(0,10);
     cikl10++;
     console.log(uzd10, cikl10);
 }
+console.log('---------------------11----------------------------');
+let uzd11 = 0;
+let sum11=0;
+let cikl11=0;
+
+while (sum11 <20 || cikl11 <11){
+    cikl11++;
+    uzd11 = rand(0,10);
+    console.log(uzd11);
+    sum11=sum11+uzd11;
+}
+console.log(sum11, cikl11)
