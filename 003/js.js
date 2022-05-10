8809ed26d76dcb080be3ab550e300581fb63b697
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-10 08:32:54 +0300

    nd done

--- 003/js.js
+++ 003/js.js
@@ -1,55 +1,96 @@
-// let a =0;
-// while (true){
-// if (++a >100){
-//     console.log('avarija');
-//     break;
-// }
-// }
 
+let a = 0;
 
-// for(let i=0;i<10;i++){
-//     if(1==2){
-//         break;
-//     }
-//     if ( i == 6-1){
-//         continue ;  // nuttraukia esama rata
-//     }
-//     console.log(`dabar sukasi ${i+1} ratas`);
-// }
-// let b;let c;
+let b = 0;
 
-// do {
-//     if (a++ >100){
-//         console.log('avarija');
-//         break;
-//     }
+while (b < -10) {
 
-// }while(c < -10);
-
-// console.log(b,c);
-
-
-for (let i=1;i<5;i++){
-    console.log(`sukasi didyaia ${i+1} ciklas`)
-    for (let k=1;k<5;k++){
-        console.log(`sukasi vidinis ${k+1} ciklas`)
+    if (++a > 100) {//saugiklis
+        console.log('avarija');
+        break; 
     }
+
+    b++;
+
 }
 
-console.log('----------_______________-------------____________-------------');
-function rand(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
+let c = 0;
+
+do {
+
+    if (++a > 100) {//saugiklis
+        console.log('avarija');
+        break; 
+    }
+
+    c++;
+
+
+}while(c < -10);
+
+console.log(b, c);
+
+for (let i = 0; i < 10; i++) {
+
+    if (i == 2) {
+        break;
+    }
+    
+    if (i == 6 || i == 3) {
+        continue;
+    }
+    
+    console.log(`Dabar sukasi:
+     ${ i + 1 } ratas`);
+
 }
 
-const kotletoKasniai = rand(10,30)
-let kotletasSuvalgytasPerKrimst =0;
+console.log(['','','','',''].length);
 
-for (let i=0;i<kotletoKasniai;i++){
-    // const gabaliukasSukramtomas =rand(1,10);
-    // kotletasSuvalgytasPerKrimst+=gabaliukasSukramtomas;
 
-    do{
-        kotletasSuvalgytasPerKrimst++;
-    }while(rand(0,4))
+for (let i = 0; i < 5; i++) {
+    console.log(`Dabar einasi DIDELIS: ${ i + 1 } ratas`);
+
+    for (let k = 0; k < 3; k++) {
+        console.log(`Dabar einasi MAZIUKAS: ${ k + 1 } ratukas`);
+    }
+
 }
-console.log(kotletasSuvalgytasPerKrimst);
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// leksteje gali buti 10...30 kasniu kotleto
+// kasnis susikramto per rand 1...10
+// kasnis susikramto neaisku kaip- tikrinti po kiekvieno krimstelejimo
+
+const kotletoKasniai = rand(10, 30);
+let kotletasSuvalgytasPer = 0;
+
+for (let i = 0; i < kotletoKasniai; i++) {
+ 
+    // const gabaliukasSukramtomas = rand(1, 10);
+    // kotletasSuvalgytasPer += gabaliukasSukramtomas;
+
+    let r;
+    do {
+        r = rand(0, 2);
+        kotletasSuvalgytasPer++;
+
+    } while(r);
+
+
+}
+
+console.log(kotletasSuvalgytasPer);
+
+
+
+
+
+
+
+
--- nd2,1/basicloops.js
+++ nd2,1/basicloops.js
@@ -39,3 +39,55 @@ while (sum11 <20 || cikl11 <11){
     sum11=sum11+uzd11;
 }
 console.log(sum11, cikl11)
+
+console.log('---------------------12----------------------------');
+let uzdrnd12 =0;
+let nelyg12=0;
+
+while (nelyg12 < 3){
+    uzdrnd12= rand(0,10);
+    console.log(uzdrnd12);
+    if (uzdrnd12 %2=== 1){
+        nelyg12++;
+    }
+}
+
+console.log('---------------------13----------------------------');
+
+let uzd13a =0;let uzd13b=1;
+while(uzd13a != uzd13b){
+    uzd13a= rand(0,10);
+    uzd13b= rand(0,10);
+    console.log(uzd13a,"  ", uzd13b);
+}
+
+console.log('---------------------14----------------------------');
+let sk14a =0; let sk14b=0;
+let sum14a=0; let sum14b=0;
+while (sum14a <100 && sum14b<100){
+    sk14a = rand(0,10);
+    sk14b= rand(0,10);
+    sum14a+=sk14a;
+    sum14b+=sk14b;
+    console.log(sum14a," ",sum14b)
+}
+
+
+console.log('---------------------15----------------------------');
+
+
+let uzdrnd15a =0;
+let uzdrnd15b =0;
+let nelyg15a=0;
+let nelyg15b=0;
+
+while (nelyg15a < 3 || nelyg15b<3){
+    uzdrnd15a= rand(0,10);  uzdrnd15b= rand(0,10);
+    console.log(uzdrnd15a," ",uzdrnd15b);
+    if (uzdrnd15a %2=== 1){
+        nelyg15a++;
+    }
+    if (uzdrnd15b %2=== 1){
+        nelyg15b++;
+    }
+}
