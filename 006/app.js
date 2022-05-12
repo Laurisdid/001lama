ee699bb0f451353d768989eadd788c0ed5e848cc
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-12 14:28:23 +0300

    paskaitos galas

--- 006/app.js
+++ 006/app.js
@@ -47,3 +47,58 @@ console.log(du());
 console.log(vienas());
 
 
+
+
+
+// const fancy =()=>{
+// return kita=()=>{
+//     console.log('labas')
+// }
+// }
+
+const fancy = ()=>()=> console.log('labas');
+fancy()();
+
+function alio(element){
+    console.log(element);
+}
+
+const array = ['a','b','c'];
+//array.forEach((element,i) => console.log(element,i));
+    
+const dv =[
+    [2,3,0,8,0],
+    [7,3,8,5,6],
+    [9,3,9,8,0],
+    [1,7,8,8,9]
+]
+// dv.forEach(r=>navigator.forEach(n=>console.log(n)));
+
+
+
+const cat={
+    name:'pilkis',
+    voice: function(){console.log('meu '+ this.name)}
+}
+cat.voice();
+
+//savybes kurios yra funkijos tai metodai
+
+
+const n = ["Petras", "Asilas", "Barsukas", "Kate"];
+
+
+n.sort((a,b)=>{
+if(a>b){
+    return 1;
+}
+if(a<b){
+    return -1;
+}
+return 0;
+});
+
+
+console.log(n)
+
+
