f9882f74eab814906703a13ed16d16c1eb97e5d1
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-17 09:47:16 +0300

    m

---
+++ JBond/jsfunkcplus.js
@@ -0,0 +1,13 @@
+// https://docs.google.com/document/d/1W_PXawhBTbCNqwmNOpisJWkxXnrTUtOV9-owb8Ufdrc/edit
+
+function funkija(){
+ console.log('grazino')
+}
+console.log(funkija())
+
+function funkija2(arg,sk){
+  arg='labas'
+    console.log(arg)
+sk++;
+   }
+   console.log(funkija2());
